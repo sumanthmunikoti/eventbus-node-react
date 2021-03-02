ee7c7f43240746264717900266e3bdd62ded6d9e
sumanthmunikoti <sumanthmunikoti1781989@gmail.com>
2021-03-02 12:54:15 -0500

    Init commit

---
+++ event-bus/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+
+const app = express();
+app.use(bodyParser.json());
+
+const events = []
+
+app.post('/events', (req, res) => {
+  const event = req.body;
+
+  //push all receiving events to the 'events' array
+  events
+
+  axios.post('http://localhost:4000/events', event);
+  axios.post('http://localhost:4001/events', event);
+  axios.post('http://localhost:4002/events', event);
+  axios.post('http://localhost:4003/events', event);
+
+  res.send({ status: 'OK' });
+});
+
+app.listen(4005, () => {
+  console.log('Listening on 4005');
+});
